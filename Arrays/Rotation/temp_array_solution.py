91b7d5a209520b64b6a56d8926e52f8f3ba8fb17
ved_k <vedant.kakade@iauro.com>
2020-01-30 14:39:26 +0530

    Modified: rectored temp array soltution technique for array rotation

--- Arrays/Rotation/temp_array_solution.py
+++ Arrays/Rotation/temp_array_solution.py
@@ -1,21 +1,24 @@
 # Array rotation using temperory array technique
 def rotate(arr, size, rotateBy):
-    tempArr = []
-    # add elements into temp array of size 'rotateBy'
-    for i in range(0, rotateBy):
-        tempArr.append(arr[i])
+    if size > 0:
+        tempArr = []
+        # add elements into temp array of size 'rotateBy'
+        for i in range(0, rotateBy):
+            tempArr.append(arr[i])
 
-    # shift rest of the array
-    shiftIndex = 0
-    for i in range(rotateBy, size):
-        arr[shiftIndex] = arr[i]
-        shiftIndex += 1
+        # shift rest of the array
+        shiftIndex = 0
+        for i in range(rotateBy, size):
+            arr[shiftIndex] = arr[i]
+            shiftIndex += 1
 
-    # store the temp array elements at the end of array
-    shiftBackIndex = size - rotateBy
-    tempIndex = 0
-    for i in range(shiftBackIndex, size):
-        arr[i] = tempArr[tempIndex]
-        tempIndex += 1
+        # store the temp array elements at the end of array
+        shiftBackIndex = size - rotateBy
+        tempIndex = 0
+        for i in range(shiftBackIndex, size):
+            arr[i] = tempArr[tempIndex]
+            tempIndex += 1
 
-    return arr
+        return arr
+    else:
+        return []
