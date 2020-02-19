e889cb125b3efa7d598449bfff9665cd85dbf3d0
vedk21 <vedantkakade.cp@gmail.com>
2020-02-20 02:31:51 +0530

    Algorithm: find kth largest element in array using quick select method

--- Arrays/Statistics/kth_largest_element_solution.py
+++ Arrays/Statistics/kth_largest_element_solution.py
@@ -43,7 +43,7 @@ def heapify(arr, size):
     return arr
 
 def kthLargestUsingHeapify(arr, size, k):
-    if k > size:
+    if k < 0 or k > size:
         return None
     else:
         arr = heapify(arr, k)
@@ -58,6 +58,50 @@ def kthLargestUsingHeapify(arr, size, k):
     
     return arr[0]
 
+# Find kth largest element in array using quick select method
+# Time Complexity: O(n2) and Theta(n) average case
+# Auxiliary Space Complexity: O(1)
+def findPivotIndex(arr, start=0, end=None):
+    if end is None:
+        end = len(arr) - 1
+
+    pivot = arr[start]
+    pivotIndex = start
+    # find the appropriate pivot index and return
+    for i in range(start + 1, end + 1):
+        if arr[i] < pivot:
+            # increase pivot index until we find numbers less than pivot element
+            pivotIndex += 1
+            arr[i], arr[pivotIndex] = arr[pivotIndex], arr[i]
+
+    return pivotIndex
+
+def quick_sort(arr, k, size, left=0, right=None,):
+    if right is None:
+        right = len(arr) - 1
+
+    if left < right:
+        pivotIndex = findPivotIndex(arr, left, right)
+
+        # put the pivot element at pivot index
+        arr[left], arr[pivotIndex] = arr[pivotIndex], arr[left]
+
+        # if pivotIndex is k then return arr[pivotIndex]
+        if (size - pivotIndex) == k:
+            return arr[pivotIndex]
+        # if pivotIndex is lesser than k then recur left subarray
+        if (size - pivotIndex) < k:
+            return quick_sort(arr, k, size, left, pivotIndex)
+        # if pivotIndex is greater than k then recur right subarray
+        if (size - pivotIndex) > k:
+            return quick_sort(arr, k, size, pivotIndex+1, right)
+
+def kthLargestUsingQuickSelect(arr, size, k):
+    if k > 0 and k <= size:
+        return quick_sort(arr, k, size, 0, size - 1)
+    else:
+        return None
+
 # helper function
 def printArray(arr, msg):
     print(msg, end="")
@@ -70,8 +114,9 @@ def printArray(arr, msg):
 
 if __name__ == '__main__':
     arr = np.array([5, 3, 10, 8, 14, 0], dtype='int')
-    k = 5
+    k = 1
     printArray(arr, 'Original Array: ')
     # Find kth largest element in array using healpfy technique
     # Technique 1
-    print(f'The {k}th largest element in array using heapify method is: {kthLargestUsingHeapify(arr, len(arr), k)}')
+    print(f'The {k}th largest element in array using heapify method is: {kthLargestUsingHeapify(np.copy(arr), len(arr), k)}')
+    print(f'The {k}th largest element in array using quick select method is: {kthLargestUsingQuickSelect(np.copy(arr), len(arr), k)}')
